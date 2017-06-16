056b156ece5fb840741f38b39f0831bc99b6c684
Marcus Svendstorp <jungiebumper@gmail.com>
2017-06-16 09:16:15 +0200

    Added driver script example and changed main.m from a script to a function

--- main.m
+++ main.m
@@ -1,63 +1,43 @@
-%% Using servicetime = exponential, betweentime = exponential
+function O = main(D, N)
+if N.maxQueueLength==0 && N.isBreakPossible
+    error('isBreaksPossible is true and maxQueueLength is 0. Breaks are not possible when maxQueueLength is 0.')
+end
 
-maxPreSpace = 10000;
-maxQueueLength = 0;
-numServers = 10;
-isCommonQueue = 1;  %Set to for a single common queue. Set to 0 for many queues, i.e. one queue for each server
-probManyItems = 0;
+blockedCounts = zeros(N.numExperiments,1);
+eventCounts = zeros(N.numExperiments,1);
+serversOccupiedTimes = zeros(N.numExperiments, N.numServers);
+customerCounts = zeros(N.numExperiments,1);
+queueTimes = cell(N.numExperiments,1);
 
-D = struct();
-D.fewItemsDist = @() exprnd(8);            % mean service time for self
-D.manyItemsDist = @() exprnd(1);          % mean service time for normal
-
-% serviceDist = @() 1;                  % constant
-% serviceDist = @() 1*rand^(-1/2.05);   % pareto beta=1, k=2.05
-
-
-D.arrivalDist = @() exprnd(1);            % mean inter arrival time
-
-
-numExperiments = 10;
-blockedCounts = zeros(numExperiments,1);
-eventCounts = zeros(numExperiments,1);
-serversOccupiedTimes = zeros(numExperiments, numServers);
-customerCounts = zeros(numExperiments,1);
-%The store is open for 14 hours
-maxT = 60*14*12;
-% Burn in period of 1 hour
-burnInPeriod = 60*14;
-queueTimes = cell(numExperiments,1);
-rng(1);
-
-for i=1:numExperiments
-
-    lists = initialize(maxPreSpace, numServers, D,isCommonQueue);
-
+for i=1:(N.numExperiments)
+    
+    lists = initialize(N.maxPreSpace, N.numServers, D, N.isCommonQueue);
+    
     nextEvent = lists.events.next();
     countStabilizer = 0;
     % Simulating discrete event
-    while (nextEvent.timeStamp < maxT)
+    while (nextEvent.timeStamp < N.maxT)
         
-        if countStabilizer > 20 && maxQueueLength ~= 0
-        countStabilizer = 0;
-        if max(lists.queue.getQueueSizes()) > 0.7*maxQueueLength && sum(lists.breakOn) >= 1
-            event = struct('type','BreakOff','timeStamp', nextEvent.timeStamp+eps);
-            lists.events.addToEventList(event);
-            disp('BreakOff')
-        end
-        if max(lists.queue.getQueueSizes()) < 1 && sum(lists.breakOn)< numServers-1
-            event = struct('type','BreakOn','timeStamp', nextEvent.timeStamp+eps);
-            lists.events.addToEventList(event);
-            disp('BreakOn')
-        end
+        if countStabilizer > 20 && N.maxQueueLength ~= 0 && N.isBreakPossible
+            countStabilizer = 0;
+            if max(lists.queue.getQueueSizes()) > N.breakThresholds(1)*N.maxQueueLength && sum(lists.breakOn) >= 1
+                event = struct('type','BreakOff','timeStamp', nextEvent.timeStamp+eps);
+                lists.events.addToEventList(event);
+                disp('BreakOff')
+            end
+            if max(lists.queue.getQueueSizes()) < N.breakThresholds(2) && sum(lists.breakOn)< N.numServers-1
+                event = struct('type','BreakOn','timeStamp', nextEvent.timeStamp+eps);
+                lists.events.addToEventList(event);
+                disp('BreakOn')
+            end
         end
         switch nextEvent.type
             case 'Arrival'
                 [lists,block] = arrive(lists, D, nextEvent.timeStamp,...
-                                        maxQueueLength,probManyItems);
-
+                    N.maxQueueLength, N.probManyItems);
+                
                 %Gathering statistical data
-                if nextEvent.timeStamp > burnInPeriod
+                if nextEvent.timeStamp > N.burnInPeriod
                     customerCounts(i) = customerCounts(i) + 1;
                 else
                     block = 0;
@@ -70,13 +50,13 @@ for i=1:numExperiments
                 %for servers)
                 queueTimes{i} = [queueTimes{i} queueTime];
                 
-                if nextEvent.timeStamp > burnInPeriod
+                if nextEvent.timeStamp > N.burnInPeriod
                     serverIdx = nextEvent.payload.serverIdx;
                     serversOccupiedTimes(i, serverIdx) = ...
-                                                         serversOccupiedTimes(i, serverIdx) + ...
-                                                         lists.servers.timeOccupied(serverIdx);
+                        serversOccupiedTimes(i, serverIdx) + ...
+                        lists.servers.timeOccupied(serverIdx);
                 end
-
+                
             case 'BreakOn'
                 idx = find(lists.breakOn==0);
                 lists.breakOn(idx(1)) = 1;
@@ -90,28 +70,18 @@ for i=1:numExperiments
         
         blockedCounts(i) = blockedCounts(i) + block;
         block = 0;
-        eventCounts(i) = eventCounts(i) + 1;        
+        eventCounts(i) = eventCounts(i) + 1;
         nextEvent = lists.events.next();
         countStabilizer = countStabilizer + 1;
     end
     disp(i)
 end
-%%
-% Print blocking fractions for all experiments and mean blocking fraction
-disp((blockedCounts./customerCounts)')
-disp(' ')
-disp(mean(blockedCounts./customerCounts))
 
-subplot(1,2,1)
-histogram(queueTimes{1}(queueTimes{1} ~= 0))
-title('Histogram of queue times')
-xlabel('Queue time (minutes)')
-ylabel('Frequency')
-set(gca,'Fontsize',14)
-subplot(1,2,2)
-bar(serversOccupiedTimes(1,:)/maxT)
-title('Bar plot of "server efficiency"')
-ylim([0 1])
-xlabel('Server index')
-ylabel('Occupied time / total time')
-set(gca,'Fontsize',14)
+O = struct();
+O.blockedCounts = blockedCounts;
+O.customerCounts = customerCounts;
+O.eventCounts = eventCounts;
+O.queueTimes = queueTimes;
+O.serversOccupiedTimes = serversOccupiedTimes;
+
+end
