8d278914536f0ed1e214d2bc42c16e8b695c5608
Marcus Svendstorp <jungiebumper@gmail.com>
2017-06-19 09:17:30 +0200

    Added nicer filename when saving data in driverVanilla.m
    Former-commit-id: 3f8419236a0f3cbcf52e689282c3138c3a432ec6

--- Drivers/driverVanilla.m
+++ Drivers/driverVanilla.m
@@ -54,7 +54,8 @@ for i = 1:length(serviceTimeMeans)
 end
 %%
 clear i j D O N
-save('FirstExperiment-1458-16-06')
+c = clock;
+save(sprintf('Drivers/driverVanillaExp-%d-%d-%d-%d-%d',c(1),c(2),c(3),c(4),c(5)))
 %% Print and plot output
 % Print blocking fractions for all experiments and mean blocking fraction
 % stringToPrint = sprintf('%.3f ', (O.blockedCounts./O.customerCounts)');
